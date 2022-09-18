458828ba6d54c1c0acb4ec13d8fc7a4ae4a1b008
achyutagupta <107514366+achyutagupta@users.noreply.github.com>
2022-09-19 00:08:35 +0530

    compiling 2

--- Achyuta Files/js/color-settings.js
+++ Achyuta Files/js/color-settings.js
@@ -1,32 +1,32 @@
-(function($) {
+(function ($) {
     "use strict";
 
-    $('.color-trigger').on('click', function() {
+    $('.color-trigger').on('click', function () {
         $(this).parent().toggleClass('visible-palate');
     });
-
-    var layoutChangerBtn = $(".color-palate .box-version li");
-    var body = $("body");
-    layoutChangerBtn.on("click", function(e) {
+	
+	var layoutChangerBtn = $(".color-palate .box-version li");
+	var body = $("body");
+	layoutChangerBtn.on("click", function(e) {
         var $this = $(this);
-        if ($this.hasClass("box")) {
+        if ( $this.hasClass("box") ) {
             body.addClass("box-layout");
         } else {
-            body.removeClass("box-layout");
-        };
-    });
+        	body.removeClass("box-layout");
+    	};
+	});
 
 
-    var directionChanger = $(".color-palate .rtl-version li");
-    var wrapper = $(".page-wrapper");
-    directionChanger.on("click", function(e) {
+	var directionChanger = $(".color-palate .rtl-version li");
+	var wrapper = $(".page-wrapper");
+	directionChanger.on("click", function(e) {
         var $this = $(this);
-        if ($this.hasClass("rtl")) {
+        if ( $this.hasClass("rtl") ) {
             wrapper.addClass("rtl");
         } else {
-            wrapper.removeClass("rtl");
-        };
-    });
+        	wrapper.removeClass("rtl");
+    	};
+	});
 
 
 
